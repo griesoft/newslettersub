5f262fbf5e0cac1eb62e1198f391e23fb8d94efd
Joonas Griesinger <griesinger.joonas@gmail.com>
2020-02-08 14:32:51 +0200

    Add a NewsletterSubModelBuilder extension method for more convenient package setup.

---
+++ src/Data/NewsletterSubModelBuilder.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NewsletterSub.Data;
+
+namespace Microsoft.EntityFrameworkCore
+{
+    public static class NewsletterSubModelBuilder
+    {
+        public static void BuildNewsletterSubModels(this ModelBuilder modelBuilder)
+        {
+            _ = modelBuilder ?? throw new ArgumentNullException(nameof(modelBuilder));
+
+            modelBuilder.Entity<Contact>()
+                .Property(c => c.CreatedAt)
+                .HasDefaultValueSql("getdate()");
+
+            modelBuilder.Entity<Contact>()
+              .HasIndex(c => c.Email)
+              .IsUnique();
+
+            modelBuilder.Entity<NewsletterSubscription>()
+              .Property(c => c.CreatedAt)
+              .HasDefaultValueSql("getdate()");
+        }
+    }
+}
